e350f980e6120a134171e71273cb7b3aeda6e18c
TDH55 <taylorhoward1@wustl.edu>
2021-04-16 00:16:35 -0500

    Added functions to login to twitter api and get user info based on a screen name. Also added run folder to .gitignore because that is where I loaded env files for now

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
 	"log"
 	"net/http"
+	"os"
 )
 
 // InitLibs prepares external libraries with credentials to make API calls.
-func InitLibs() {
+func InitLibs() (*twitter.Client, error) {
 	// TODO: Use environment variables to initialize twitter and google libs
+	//get a twitter client
+	twitterCredentials := TwitterCredentials{
+		AccessToken:       os.Getenv("ACCESS_TOKEN"),
+		AccessTokenSecret: os.Getenv("ACCESS_TOKEN_SECRET_KEY"),
+		ConsumerKey:       os.Getenv("API_KEY"),
+		ConsumerSecret:    os.Getenv("API_SECRET_KEY"),
+	}
+
+	client, err := getTwitterClient(&twitterCredentials)
+	if err != nil {
+		fmt.Println("error getting twitter client")
+		return nil, err
+	}
+
+	//fmt.Printf("%+v\n", client)
+	return client, nil
 }
 
 // Health is a probe endpoint, it always returns StatusOK and "Healthy".
@@ -18,7 +37,23 @@ func Health(w http.ResponseWriter, r *http.Request) {
 // main starts up the webserver.
 func main() {
 	// TODO: Use environment variables for address and credentials
-	InitLibs()
+	client, err := InitLibs()
+	if err != nil {
+		return
+	}
+
+	//This is temporary, get user function will eventually be called based on user input
+	user, err := getUser(*client, "tdhoward55")
+
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+	fmt.Println("user: ")
+	//_ = user
+	fmt.Println(user.ScreenName)
+	//end of get user testing
+
 	// Handle requests for static files
 	http.Handle("/static", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 	// Handle calls to the analysis endpoint
--- twitter.go
+++ twitter.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+// TwitterCredentials data structore for twitter api credentials
+type TwitterCredentials struct {
+	ConsumerKey       string
+	ConsumerSecret    string
+	AccessToken       string
+	AccessTokenSecret string
+}
+
+//getTwitterClient function to authorize twitter api and create a client
+func getTwitterClient(credentials *TwitterCredentials) (*twitter.Client, error) {
+	config := oauth1.NewConfig(credentials.ConsumerKey, credentials.ConsumerSecret)
+	token := oauth1.NewToken(credentials.AccessToken, credentials.AccessTokenSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+
+	verifyParams := &twitter.AccountVerifyParams{
+		SkipStatus:   twitter.Bool(true),
+		IncludeEmail: twitter.Bool(true),
+	}
+
+	user, _, err := client.Accounts.VerifyCredentials(verifyParams)
+	if err != nil {
+		return nil, err
+	}
+	//log.Printf("user's account:\n%+v\n", user)
+	_ = user //silencing an error for now
+	return client, nil
+}
+
+//getUser get user info based on the user input (screen name)
+func getUser(client twitter.Client, username string) (*twitter.User, error) {
+	includeEntities := true
+	searchParams := twitter.UserSearchParams{
+		Query:           username,
+		Page:            1,
+		Count:           1,
+		IncludeEntities: &includeEntities,
+	}
+	users, _, err := client.Users.Search(username, &searchParams)
+	if err != nil {
+		fmt.Println("error finding user")
+		return nil, err
+	}
+	fmt.Println(users)
+	return &users[0], nil
+}
+
 // Tweets gets a list of tweets specified by data
 func Tweets(data *GetAnalysisData) ([]string, error) {
 	tweets := make([]string, 0)
