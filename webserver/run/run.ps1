b36d654bc0d4c038dbf51c3fe6093b6400ca4a9b
Riley McCuen <riley.mccuen@wustl.edu>
2021-04-26 15:08:38 -0500

    fixed rate limiting issue and other bugs

--- webserver/run/run.ps1
+++ webserver/run/run.ps1
@@ -1,11 +1,20 @@
 # Builds the go application, runs it, then opens a new tab in the browser
-$env:ACCESS_TOKEN = ""
-$env:ACCESS_TOKEN_SECRET_KEY = ""
-$env:API_KEY = ""
-$env:API_SECRET_KEY = ""
-$env:GOOGLE_APPLICATION_CREDENTIALS = ""
+# Twitter env variables
+$env:ACCESS_TOKEN = "1094720198433783811-g3SeuryIvlgyXtnX84i1SDn5TuiYtK"
+$env:ACCESS_TOKEN_SECRET_KEY = "Z7i0pLVs0AKeV0yChekuFQblBgmoNMthqsACqrb6xGISc"
+$env:API_KEY = "RgiKQBtQQRhV0d0vngYfGRuR9"
+$env:API_SECRET_KEY = "r2OwDZQKy7TX63YoiWqmojTnEUxVvnzgH1YjwnSCzSEPUtpZij"
+# Google env variables
+$env:GOOGLE_APPLICATION_CREDENTIALS = "../twitteranalytics-310723-f09ac30d22c2.json"
+$env:BUCKET = "twittertimelines"
+$env:PROJECT_ID = "twitteranalytics-310723"
+$env:PUB_SUB_TOPIC_ID = "twitter-fetch"
+# App config env variables
 $env:ADDRESS = "0.0.0.0:80"
-go build -o ../build/twitteranalytics.exe .
-Start-Process -FilePath Chrome -ArgumentList localhost/static
+# Build the app
+go build -o ../build/twitteranalytics.exe ..
+# Open a tab in the browser pointed at the webserver
+Start-Process -FilePath "Chrome" -ArgumentList "localhost/static/"
+# Run the app
 Write-Output "Running..."
 ../build/twitteranalytics.exe
