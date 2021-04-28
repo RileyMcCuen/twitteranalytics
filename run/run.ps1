2ea7c016701c06a8d7a470958ee26c9c8608408a
Riley McCuen <rileymccuen@yahoo.com>
2021-04-28 13:04:06 -0500

    initial kubernetes files set up

--- run/run.ps1
+++
@@ -1,5 +0,0 @@
-# Builds the go application, runs it, then opens a new tab in the browser
-go build -o ../build/twitteranalytics.exe .
-Start-Process -FilePath Chrome -ArgumentList localhost/static
-Write-Output "Running..."
-../build/twitteranalytics.exe
--- twitter/run/run.ps1
+++ twitter/run/run.ps1
@@ -6,7 +6,7 @@ $env:API_SECRET_KEY = ""
 $env:GOOGLE_APPLICATION_CREDENTIALS = ""
 $env:BUCKET = "twittertimelines"
 $env:PUB_SUB_SUBSCRIPTION_ID = "twitter-fetch"
-$env:PUB_SUB_TOPIC_ID = "twitter-documents"
+$env:PUB_SUB_PUBLISH_ID = "twitter-documents"
 $env:ADDRESS = "0.0.0.0:8001"
 go build -o ../build/twitter.exe ..
 Write-Output "Running..."
